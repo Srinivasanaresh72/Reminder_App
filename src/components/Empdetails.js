8f2fa259eeaf0d171e8bf9948f6a2ef5b9e99b7d
Naresh <srinivasanaresh72@gmail.com>
2021-12-13 20:14:41 +0530

    added css for employee details

--- src/components/Empdetails.js
+++ src/components/Empdetails.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import { Link } from "react-router-dom";
 
 const Empdetails = () => {
     return (
-        <div>
-            <h1> Employee Details Page </h1>
+        <div className="container">
+            <h3> Employee Birthday Details </h3>
+            <Link to="/empbirthday"> Today Birthday </Link>
         </div>
     )
 }
