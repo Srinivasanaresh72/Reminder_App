1dc25064edf7e6a23cc5b75ed74617161c93908c
Naresh <srinivasanaresh72@gmail.com>
2021-12-13 15:26:51 +0530

    change

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import React from 'react'
+import React from "react";
+import Home from './components/Home';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} 
+from "react-router-dom";  
 
 const App = () => {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/" component={Home} />
+      </Switch>
+    </Router>
+
   );
 }
 
