83d7887728f6a797310d4a04a67973edf67ca808
Naresh <srinivasanaresh72@gmail.com>
2021-12-13 16:59:34 +0530

    added home,details,birthday pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React from 'react'
 import Home from "./components/Home";
+import Empdetails from './components/Empdetails';
+import Empbirthday from './components/Empbirthday';
+import Error from './components/Error';
 
 import {
   BrowserRouter as Router,
@@ -7,11 +10,15 @@ import {
   Route
 } from "react-router-dom"; 
 
+
 const App = () => {
   return (
     <Router>
       <Switch>
-        <Route path="/" component={Home} />
+        <Route path="/" component={Home} exact/>
+        <Route path="/empdetails" component={Empdetails} exact/>
+        <Route path="/empbirthday" component={Empbirthday} exact/>
+        <Route component={Error} />
       </Switch>
     </Router>
   );
---
+++ src/components/Empbirthday.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Empbirthday = () => {
+    return (
+        <div>
+            <h1> Employee Birthday Page </h1>
+        </div>
+    )
+}
+
+export default Empbirthday
